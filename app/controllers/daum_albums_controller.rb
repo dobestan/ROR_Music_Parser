1d4d4392cdf36c663f87a78e50fc482046223662
dobestan <dobestan@gmail.com>
2014-02-17 17:49:57 +0900

    seperate action

--- app/controllers/daum_albums_controller.rb
+++ app/controllers/daum_albums_controller.rb
@@ -1,9 +1,15 @@
 class DaumAlbumsController < ApplicationController
   def index
+    @daum_albums = DaumAlbum.all
+  end
+
+  def new
     @daum_albums = DaumAlbum.get_albums("http://music.daum.net/chart/chartsong/list")
 
     @daum_albums.each do |daum_album|
       DaumAlbum.create(title: daum_album["title"], artist: daum_album["artist"], youtube_hash: daum_album["youtube_hash"])
     end
+
+    render 'index'
   end
 end
--- app/controllers/naver_albums_controller.rb
+++ app/controllers/naver_albums_controller.rb
@@ -1,9 +1,14 @@
 class NaverAlbumsController < AlbumsController
   def index
-    @naver_albums = NaverAlbum.get_albums("http://music.naver.com/todayMusic/index.nhn?startDate=20140210")
+    @naver_albums = NaverAlbum.all
+  end
 
+  def new
+    @naver_albums = NaverAlbum.get_albums("http://music.naver.com/todayMusic/index.nhn?startDate=20140210")
     @naver_albums.each do |naver_album|
       NaverAlbum.create(title: naver_album["title"], artist: naver_album["artist"], youtube_hash: naver_album["youtube_hash"])
     end
+
+    render 'index'
   end
 end
